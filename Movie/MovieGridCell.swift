b00c03ce08769dd023b7daf6631c3330ee3642a0
Giovanni Rosas-Marin <marin@Giovannis-MacBook-Pro.local>
2019-02-17 23:41:27 -0800

    done

---
+++ Movie/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  Movie
+//
+//  Created by Giovanni Rosas-Marin on 2/17/19.
+//  Copyright © 2019 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+
+}
